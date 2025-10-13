e8f466b66b229c0afef2da12442dd559d9e5441f
Ruan Vinicius <ruanvinciius01@gmail.com>
2025-10-13 00:29:00 -0300

    Adicionando função de soma

--- src/App.js
+++ src/App.js
@@ -6,16 +6,29 @@ import Input from './components/Input';
 import {Container, Content, Row,} from './styles';
 import { useState } from 'react';
 
-
-
 const App = () => {
   const [currentNumber, setCurrentNumber] = useState(0);
+  const [firstNumber, setFirstNumber] = useState('0');
 
   const handleClear = () => {
     setCurrentNumber('0');
+    setFirstNumber('0');
   };
+  
   const handleAddNumber = (number) => {
-    setCurrentNumber(prev => `${number} ${prev === '0' ? '' : prev}`);
+    setCurrentNumber(prev => ` ${prev === '0' ? '' : prev}${number}`);
+  }
+
+  const handleSumNumbers = () => {
+
+    if(firstNumber === '0'){
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+    } else {
+      const sum = Number(firstNumber) + Number(currentNumber);
+      setCurrentNumber(String(sum));
+      setFirstNumber('0');
+    }
   }
   return (
     <Container>
@@ -25,7 +38,7 @@ const App = () => {
         <Button label="*" onclick={() => handleAddNumber('')} />
         <Button label="/" onclick={() => handleAddNumber('')} />
         <Button label="C" onclick={handleClear} />
-        <ButtonYellow label="X" onclick={() => handleAddNumber('')} />
+        <ButtonYellow label="%" onclick={() => handleAddNumber('')} />
         </Row>
         <Row>
         <Button label="7" onclick={() => handleAddNumber('7')} />
@@ -37,7 +50,7 @@ const App = () => {
         <Button label="4" onclick={() => handleAddNumber('4')} />
         <Button label="5" onclick={() => handleAddNumber('5')} />
         <Button label="6" onclick={() => handleAddNumber('6')} />
-        <ButtonYellow label="+" onclick={() => handleAddNumber('+')} />
+        <ButtonYellow label="+" onclick={handleSumNumbers} />
         </Row>
         <Row>
         <Button label="1" onclick={() => handleAddNumber('1')} />
